e7ff81d90243b8b6c12374cdb453348afd29c35e
Darren Tang <darren.tang@tufts.edu>
2020-06-03 23:06:00 -0400

    Added scripts to collectibles to make them collectible

---
+++ HRI Test Environment/Assets/Scripts/Collectible.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+
+    public string type;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == type)
+        {
+            target.GetComponent<Info>().collected += 1;
+            Destroy(this.gameObject);
+        }
+    }
+}
