b9fde379b726bd8af95961523dc34881dd34a58f
Dre Vivero <dre.vivero@gmail.com>
2020-06-22 13:06:20 -0500

    added timer with visual aid when collecting gems

--- HRI Test Environment/Assets/Gems/Gems Ultimate Pack/Scripts/CollectToken.cs
+++ HRI Test Environment/Assets/Gems/Gems Ultimate Pack/Scripts/CollectToken.cs
@@ -5,34 +5,62 @@ using UnityEngine.UI;
 
 public class CollectToken : MonoBehaviour
 {
-    public int countdownTime;
-    public Text countdownDisplay;
+    public float duration;
+    public Image fillImage;
 
-    float t;
+    public float t;
+
+
 
     void OnTriggerEnter (Collider other)
     {
-
         if (other.gameObject.name == "HumanAgent")
         {
             t = 0;
-
-            Debug.Log("Contact made...");
-        } 
+            fillImage.fillAmount = 0;
+            //Debug.Log("Contact made...");
+        }
     }
 
     void OnTriggerStay(Collider other)
     {
         if (other.gameObject.name == "HumanAgent")
         {
+            StartCoroutine(Timer(duration));
+
             t += Time.deltaTime;
             if(t > 5)
             {
                 Destroy(this.gameObject);
-                Debug.Log("Object Destroyed...");
+                fillImage.fillAmount = 0;
+                //Debug.Log("Object Destroyed...");
                 //ScoringSystem.theScore += 50;
 
             }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "HumanAgent")
+        {
+            t = 0;
+            fillImage.fillAmount = 0;
+        }
+    }
+
+    public IEnumerator Timer(float duration)
+    {
+        float startTime = Time.time;
+        float time = duration;
+        float value = 0;
+
+        while (Time.time - startTime < duration)
+        {
+            //time += Time.deltaTime;
+            value = t / duration;
+            fillImage.fillAmount = value;
+            yield return null;
         }
     }
 }
