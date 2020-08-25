e4eb7b3435f36f062e167812ae9252c7fbb79bd3
Darren Tang <darrenvtang@gmail.com>
2020-08-25 15:37:23 -0400

    Added foundation for battery component

--- HRI Test Environment/Assets/Scripts/AdvancedCollect.cs
+++ HRI Test Environment/Assets/Scripts/AdvancedCollect.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.AI;
 public class AdvancedCollect : MonoBehaviour
 {
     // Start is called before the first frame update
-    const float DIAMETER = 1f, CURVE_ANGLE = 3f, CURVE_DIAMETER = .3f;
+    const float DIAMETER = 1f, CURVE_ANGLE = 3f, CURVE_DIAMETER = .3f, AVOID_ANGLE = 60f;
     const int PER_PATH = 100;
+    public Battery battery;
     public GameObject[] collectibles, markers, curveMarkers;
     public GameObject marker, curveMarker, lArrow, rArrow;
     public Vector3 initPos;
@@ -14,10 +18,11 @@ public class AdvancedCollect : MonoBehaviour
     private int destPoint, pathProgress, curveNum;
     private float initAngle, destAngle, rotSpeed;
     private NavMeshAgent agent;
-    public bool obstructed;
-    private bool facing, targetAcquired, plotted, curvePlotted, done;
+    public bool obstructed, tooClose;
+    private bool facing, targetAcquired, plotted, curvePlotted;
     void Start()
     {
+        battery = GetComponent<Battery>();
         obstructed = false;
         facing = false;
         targetAcquired = false;
@@ -46,14 +51,26 @@ public class AdvancedCollect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (done) return;
-        if (!obstructed)
+        if (!battery.charging)
         {
-            unobstructedUpdate();
-        }
-        else
-        {
-            //TODO: figure out how obstruction works
+            if (!obstructed)
+            {
+                unobstructedUpdate();
+            }
+            else
+            {
+                if (tooClose)
+                {
+                    agent.isStopped = true;
+                }
+                else
+                {
+                    facing = false;
+                    //true = right, false = left
+
+                }
+                //TODO: figure out how obstruction works
+            }
         }
     }
     void unobstructedUpdate()
@@ -84,6 +101,7 @@ public class AdvancedCollect : MonoBehaviour
         agent.ResetPath();
         if (destPoint == positions.Length - 1) return;
         destPoint++;
+        if (destPoint < positions.Length) battery.NeedToGoHome(positions[destPoint]);
         targetAcquired = true;
     }
 
@@ -92,12 +110,12 @@ public class AdvancedCollect : MonoBehaviour
         if (destPoint >= positions.Length)
         {
             agent.isStopped = true;
-            done = true;
             return;
         }
+        agent.isStopped = false;
         if (!plotted && !curvePlotted)
         {
-            plotPath(transform.position, null);
+            plotPath(transform.position, positions[destPoint], null);
         }
         else
         {
@@ -136,7 +154,7 @@ public class AdvancedCollect : MonoBehaviour
         if (pathProgress == 0 || pathProgress >= sequence.Length) directional = Vector3.SignedAngle(this.transform.forward, positions[destPoint] - transform.position, Vector3.up);
         else directional = Vector3.SignedAngle(this.transform.forward, sequence[pathProgress] - transform.position, Vector3.up);
         Debug.Log(directional);
-        if (directional >= CURVE_ANGLE || directional <= -CURVE_ANGLE)
+        if (directional >= 11 || directional <= -11)
         {
             if(directional > 0)
             {
@@ -182,22 +200,22 @@ public class AdvancedCollect : MonoBehaviour
             curveMarkers[i] = Instantiate(curveMarker);
             curveMarkers[i].transform.position = curvePath[i];
         }
-        plotPath(curvePath[curveNum - 1], curvePath);
+        plotPath(curvePath[curveNum - 1], positions[destPoint], curvePath);
         curvePlotted = true;
     }
-    void plotPath(Vector3 startPoint, Vector3 [] curvePath)
+    void plotPath(Vector3 startPoint, Vector3 endPoint, Vector3 [] curvePath)
     {
-        float distance = Vector3.Distance(positions[destPoint], startPoint);
+        float distance = Vector3.Distance(endPoint, startPoint);
         sequence = new Vector3[(int) Mathf.Ceil(distance / DIAMETER)];
         markers = new GameObject[(int)Mathf.Ceil(distance / DIAMETER)];
         sequence[0] = startPoint;
-        Vector3 direction = (positions[destPoint] - startPoint) / distance; 
+        Vector3 direction = (endPoint - startPoint) / distance; 
         for (int i = 1; i < sequence.Length; i++)
         {
             sequence[i] = (direction * (DIAMETER * i)) + startPoint;
             markers[i] = Instantiate(marker);
             markers[i].transform.position = sequence[i];
-            //markers[i].SetActive(false);
+            markers[i].SetActive(false);
         }
         if (curvePath != null && curvePath.Length != 0)
         {
@@ -229,19 +247,91 @@ public class AdvancedCollect : MonoBehaviour
             markers[i].SetActive(true);
         }
     }
-    public void obstructedPath(Vector3 hitMarker)
+    public void obstructedPath(Vector3 hitMarker, bool dirToFace)
     {
-        if(Vector3.Distance(hitMarker, positions[destPoint]) < 30f)
+        GameObject [] avoidMarkers, newMarkers;
+        Vector3[] avoidPath, newPath;
+        Vector3 peakDetour, startAvoiding = new Vector3(0,0,0), stopAvoiding = new Vector3(0,0,0), direction;
+        int at, range, avoidCurveNum;
+        if(Vector3.Distance(hitMarker, positions[destPoint]) < 2f || Vector3.Distance(hitMarker, transform.position) < 2f)
         {
-            //TODO: Too close pause the robot until human leaves
-        }
-        else if(Vector3.Distance(hitMarker, transform.position) < 30f) 
-        {
-            //TODO: Human close to robot, stationary rotation and avoidance
+            //Too close defined as two sphere lengths away from goal or robot
+            tooClose = true;
         }
         else
         {
+            float angToAvoid = AVOID_ANGLE;
+            if (!dirToFace) angToAvoid *= -1;
+            Quaternion avoidDir = Quaternion.Euler(0, angToAvoid, 0);
+            direction = (hitMarker - transform.position) / Vector3.Distance(hitMarker, transform.position);
             //TODO: Arc movement around obstacle
+            range = 2;
+            at = detourMarks(hitMarker, ref startAvoiding, ref stopAvoiding, range);
+            avoidCurveNum = (int)Mathf.Floor(Mathf.Abs(AVOID_ANGLE / CURVE_ANGLE));
+            avoidPath = new Vector3[avoidCurveNum];
+            avoidMarkers = new GameObject[avoidCurveNum];
+            newPath = new Vector3[(sequence.Length - pathProgress) + avoidMarkers.Length];
+            newMarkers = new GameObject[(sequence.Length - pathProgress) + avoidMarkers.Length];
+            //building the half-ellipsoid arc
+            for (int i = 0; i < avoidCurveNum; i++)
+            {
+                Quaternion nextAngle = Quaternion.Euler(0, angToAvoid - (CURVE_ANGLE * (i + 1)), 0);
+                Vector3 nextToFace = (nextAngle * transform.forward);
+                avoidPath[i] = nextToFace * (CURVE_DIAMETER * (i + 1)) + transform.position;
+                avoidMarkers[i] = Instantiate(curveMarker);
+                avoidMarkers[i].transform.position = avoidPath[i];
+            }
+            //change the path
+            for(int i = pathProgress; i < at - range; i++)
+            {
+                newPath[i] = sequence[i];
+                newMarkers[i] = markers[i];
+            }
+            for(int i = 0; i < avoidPath.Length; i++)
+            {
+                newPath[i + (at - range)] = avoidPath[i];
+                newMarkers[i + (at - range)] = avoidMarkers[i];
+            }
+            for (int i = at + range; i < sequence.Length; i++)
+            {
+                newPath[i + (at - range) + avoidPath.Length] = sequence[i];
+                newMarkers[i + (at - range) + avoidPath.Length] = markers[i];
+            }
+            sequence = newPath;
+            markers = newMarkers;
+            obstructed = false;
         }
+    }
+    int detourMarks(Vector3 hit, ref Vector3 start, ref Vector3 end, int howFar)
+    {
+        int at = 0;
+        for (int i = 0; i < sequence.Length; i++)
+        {
+            if(sequence[i] == hit)
+            {
+                at = i;
+                start = sequence[i - howFar];
+                end = sequence[i + howFar];
+                break;
+            }
+        }
+        return at;
+    }
+
+    public void resumePath()
+    {
+
+    } 
+
+    public void batteryDetour()
+    {
+        Vector3[] addDetour = new Vector3[positions.Length + 1];
+        for (int i = destPoint + 1; i < destPoint; i++)
+        {
+            addDetour[i] = positions[i - 1];
+        }
+        addDetour[destPoint] = battery.home.transform.position;
+        positions = addDetour;
+        targetAcquired = false;
     }
 }
--- HRI Test Environment/Assets/Scripts/ProjectionCollision.cs
+++ HRI Test Environment/Assets/Scripts/ProjectionCollision.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class ProjectionCollision : MonoBehaviour
 {
-    public GameObject robot;
+    public GameObject robot, human;
     // Start is called before the first frame update
     void Start()
     {
         robot = GameObject.FindGameObjectWithTag("Robot");
+        human = GameObject.FindGameObjectWithTag("Human");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Human")
         {
+            bool dirToFace = Vector3.SignedAngle(robot.transform.position, human.transform.position, Vector3.up) > 0;
             robot.GetComponent<AdvancedCollect>().obstructed = true;
-            robot.GetComponent<AdvancedCollect>().obstructedPath(transform.position);
+            robot.GetComponent<AdvancedCollect>().obstructedPath(transform.position, dirToFace);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Human")
+        {
+            robot.GetComponent<AdvancedCollect>().obstructed = false;
+            robot.GetComponent<AdvancedCollect>().tooClose = false;
         }
     }
 }
